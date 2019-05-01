0c193a8ae797120b0fa956d3cafd8241e8500de6
ahnevskiy <aleksey.ahnevskiy@gmail.com>
2019-05-01 22:44:16 +0300

    extract helper classess

--- test_add_groups.py
+++ test_add_groups.py
@@ -3,8 +3,10 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
+from group import Group
 
-class UntitledTestCase(unittest.TestCase):
+
+class test_add_groups(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
@@ -14,10 +16,9 @@ class UntitledTestCase(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, user_name="admin", password="secret")
         self.open_groups_page(wd)
-        self.create_new_group(wd,
-                              name="Its a test group",
-                              header="My first group for test",
-                              footer="Its a discription of this group")
+        self.create_group(wd, Group(name="Its a test group",
+                                    header="My first group for test",
+                                    footer="Its a discription of this group"))
         self.return_to_groups_page(wd)
         self.logout(wd)
 
@@ -26,10 +27,9 @@ class UntitledTestCase(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, user_name="admin", password="secret")
         self.open_groups_page(wd)
-        self.create_new_group(wd,
-                              name="",
-                              header="",
-                              footer="")
+        self.create_group(wd, Group(name="",
+                                    header="",
+                                    footer=""))
         self.return_to_groups_page(wd)
         self.logout(wd)
 
@@ -39,16 +39,16 @@ class UntitledTestCase(unittest.TestCase):
     def return_to_groups_page(self, wd):
         wd.find_element_by_link_text("group page").click()
 
-    def create_new_group(self, wd, name, header, footer):
+    def create_group(self, wd, group):
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group forms
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(name)
+        wd.find_element_by_name("group_name").send_keys(group.name)
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(header)
+        wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(footer)
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
 
