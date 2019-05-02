6fdf378486970c8365df394af702fe4c35b46d20
ahnevskiy <aleksey.ahnevskiy@gmail.com>
2019-05-02 14:06:56 +0300

    remove a creation of fixture in 'conftest.py'

--- tests/test_add_contacts.py
+++ tests/test_add_contacts.py
@@ -1,17 +1,8 @@
 # -*- coding: utf-8 -*-
 __author__ = 'ahnevskiy'
 
-import pytest
 from model.objects import Contact
 from model.useful import Date
-from fixture.application import Application
-
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
 def test_add_contact(app):
