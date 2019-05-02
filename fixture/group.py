17fbd8dd8ce3cfa3ede47f77c13e193a6d835af4
ahnevskiy <aleksey.ahnevskiy@gmail.com>
2019-05-03 01:25:24 +0300

    remove a filling group forms in a separate method

--- fixture/group.py
+++ fixture/group.py
@@ -9,18 +9,21 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("group page").click()
 
-    def create(self, group):
+    def fill_group_forms(self, group):
         wd = self.app.wd
-        self.open_groups_page()
-        # init group creation
-        wd.find_element_by_name("new").click()
-        # fill group forms
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
         wd.find_element_by_name("group_header").clear()
         wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
+
+    def create(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        # init group creation
+        wd.find_element_by_name("new").click()
+        self.fill_group_forms(group=group)
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
@@ -41,17 +44,10 @@ class GroupHelper:
         wd.find_element_by_name("selected[]").click()
         # begin editing
         wd.find_element_by_name("edit").click()
-        # fill group forms
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_group_forms(group=group)
         # submit editing
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
-
 
     def open_groups_page(self):
         wd = self.app.wd
